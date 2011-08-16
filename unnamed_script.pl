591eaa15b6c958864fb4aa16c15788b12954aa1e
André Santos <andrefs@cpan.org>
2011-08-16 12:11:46 +0100

    lcb

--- unnamed_script.pl
+++ unnamed_script.pl
@@ -20,36 +20,36 @@ generate_config($repi_client_config, $rept_client_config, $rept_generic_config);
 
 sub copy_overrides {
 	my $shared_or = "$themes_root/Shared/Overrides";
-	qx{cp -r '$shared_or'/* '$site_files/'} if -e $shared_or;
+	print qx{cp -r '$shared_or'/* '$site_files/'} if -d $shared_or;
 
 	my $generic_or = "$themes_root/Generic/$generic_theme/Overrides";
-	qx{cp -r '$generic_or'/* '$site_files/'} if -e $generic_or;
+	print qx{cp -r '$generic_or'/* '$site_files/'} if -d $generic_or;
 		
 	my $client_or = "$themes_root/Instances/$client_theme/Overrides";
-	qx{cp -r '$client_or'/* '$site_files/'} if -e $client_or;
+	print qx{cp -r '$client_or'/* '$site_files/'} if -d $client_or;
 }
 
 sub generate_saas {
 	my $sass_folder = "$themes_root/Instancies/$client_theme";
+	mkdir "$site_files/public/stylesheets/";
 	foreach my $sass (<$sass_folder/*.sass>){
 		next if $sass eq 'config.sass';
 		my $css = basename($sass);
 		$css =~ s/sass$/css/;
-		$css = "$site_files/public/stylesheets/";
-		qx{sass --update '$sass':'$css' --style compressed};
+		$css = "$site_files/public/stylesheets/$css";
+		print qx{sass --update '$sass':'$css' --style compressed};
 	}
 }
 
 sub generate_config {
 	my ($repi_client_config, $rept_client_config, $rept_generic_config) = @_;
 	my $local_conf = "$site_files/config/config.yml";
-	qx{merge_yaml.pl '$repi_client_config' '$rept_client_config' '$rept_generic_config' > '$local_conf'};
+	print qx{merge_yaml.pl '$repi_client_config' '$rept_client_config' '$rept_generic_config' > '$local_conf'};
 }
 
 sub get_theme_from_config {
 	my $config_file = shift;
 	my $config = YAML::XS::LoadFile($config_file);
-	print Dumper $config;
 	return $config->{theme};
 }
 
